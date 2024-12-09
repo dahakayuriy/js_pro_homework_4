2339901a8dac405807c31d031c21bf0513203064
Yuriy Murachov <dahakayuriy@gmail.com>
2024-12-09 11:13:19 +0200

    Changed homework3

--- main.js
+++ main.js
@@ -1,9 +1,20 @@
 "use strict";
 
-const number = prompt("Enter a five-digit number:");
+const fiveDigitNumberInput = prompt("Enter a five-digit number:");
 
-alert(
-  number.length === 5 && !isNaN(number)
-    ? number.split("").join(" ")
-    : "Please enter the correct five-digit number."
-);
+if (fiveDigitNumberInput === null) {
+  alert("Hasta la vista baby");
+} else if (
+  !fiveDigitNumberInput.trim() ||
+  isNaN(fiveDigitNumberInput) ||
+  !Number.isInteger(+fiveDigitNumberInput)
+) {
+  alert("Error: invalid number");
+} else {
+  const num = Math.abs(+fiveDigitNumberInput);
+  if (String(num).length === 5) {
+    alert("Ok, you are wellcome!");
+  } else {
+    alert("Number is not 5-digit");
+  }
+}
